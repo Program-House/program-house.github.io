447441d6782bfdd90a7c63b3ff79e9cadee53e4b
Chadtech <chadstearns1@gmail.com>
2016-10-25 22:31:53 -0400

    Bringing in my boiler plate

---
+++ src/elm/View.elm
@@ -0,0 +1,11 @@
+module View exposing (..)
+
+import Html             exposing (..)
+import Html.Attributes  exposing (..)
+import Html.Events      exposing (..)
+import Types            exposing (..)
+
+
+view : Model -> Html Msg
+view m = 
+  div [ ] [ ]
